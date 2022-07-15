988181a90a69aa2d2427d1fa9d18ee4ec85cc680
Laura Cole <lauracole1900@comcast.net>
2022-07-15 10:17:00 -0600

    added pseudocode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
 console.log("Ding!")
+
+// Generates 9x9 grid (16x16 / 30x16)
+// Each grid square is a clickable button
+// Randomly distributes 10 mines (40 / 99)
+// Generates hints around each mine, depending on how many mines are touching that square
+// Listens for r-click to mark and l-click to clear
+// Game is over if a mine is l-clicked or all mines are found
+// When a mine is l-clicked, all mines show and any wrongly-marked mines show a different color
+// Time taken is the score
+// Needs a timer that counts up
+// Reset button
